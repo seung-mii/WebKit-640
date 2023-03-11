a05d4967f4e332fc16dd1c118608bb8326b43b31
withsme <tmdal9953@gmail.com>
2023-03-12 00:06:33 +0900

    [complete] day 29, Web Controller와 JPA 연동

---
+++ 06. app backend/day29/day29_ex06/src/org/comstudy/ex05/ApplicationMain.java
@@ -0,0 +1,21 @@
+package org.comstudy.ex05;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class ApplicationMain {
+
+	public static void main(String[] args) throws IOException {
+
+		String resource = "org/mybatis/example/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		
+
+	}
+}
